b88f9fd100d1b1c2b272fead5012b45d363fdae2
moco-xta <moco.xta@gmail.com>
2024-02-17 10:35:23 +0100

    Spring initialzr

---
+++ src/main/java/com/mocoxta/mocoxtabackend/MocoXtaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mocoxta.mocoxtabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocoXtaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocoXtaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mocoxta/mocoxtabackend/MocoXtaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mocoxta.mocoxtabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocoXtaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
