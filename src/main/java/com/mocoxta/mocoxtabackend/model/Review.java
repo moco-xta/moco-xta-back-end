12ef745ab9cea90938dfaec5664633f7a7d5a45b
moco-xta <moco.xta@gmail.com>
2024-03-24 18:43:40 +0100

    Models: User + Test & Review updated with Lombok

--- src/main/java/com/mocoxta/mocoxtabackend/model/Review.java
+++ src/main/java/com/mocoxta/mocoxtabackend/model/Review.java
@@ -1,23 +1,23 @@
 package com.mocoxta.mocoxtabackend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table
+@Table(name = "review")
 public class Review {
 
     @Id
-    @SequenceGenerator(
-            name = "review_sequence",
-            sequenceName = "review_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "review_sequence"
-    )
+    @GeneratedValue
     private Long id;
     private String name;
     private String email;
@@ -25,95 +25,4 @@ public class Review {
     private String review;
     private Integer numberOfStars;
     private LocalDate date;
-
-    public Review() {
-    }
-
-    public Review(String name, String email, String role, String review, Integer number_of_stars, LocalDate date) {
-        this.name = name;
-        this.email = email;
-        this.role = role;
-        this.review = review;
-        this.numberOfStars = number_of_stars;
-        this.date = date;
-    }
-
-    public Review(Long id, String name, String email, String role, String review, Integer number_of_stars, LocalDate date) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.role = role;
-        this.review = review;
-        this.numberOfStars = number_of_stars;
-        this.date = date;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getReview() {
-        return review;
-    }
-
-    public void setReview(String review) {
-        this.review = review;
-    }
-
-    public Integer getNumberOfStars() {
-        return numberOfStars;
-    }
-
-    public void setNumberOfStars(Integer numberOfStars) {
-        this.numberOfStars = numberOfStars;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    @Override
-    public String toString() {
-        return "Review{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", role='" + role + '\'' +
-                ", review='" + review + '\'' +
-                ", number_of_stars=" + numberOfStars +
-                ", date=" + date +
-                '}';
-    }
 }
