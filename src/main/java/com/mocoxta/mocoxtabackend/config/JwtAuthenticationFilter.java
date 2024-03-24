20d84f249220e428b35855b00e0484bdb7fbcb9b
moco-xta <moco.xta@gmail.com>
2024-03-24 19:21:47 +0100

    Dependencies: jjwt-api & jjwt-impl & jjwt-jackson + JwtService

--- src/main/java/com/mocoxta/mocoxtabackend/config/JwtAuthenticationFilter.java
+++ src/main/java/com/mocoxta/mocoxtabackend/config/JwtAuthenticationFilter.java
@@ -15,6 +15,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -23,10 +25,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
